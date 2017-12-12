5f5c8dcaf9d6fbfce28314b3627cdcab803cfa2d
u-suk <34409132+u-suk@users.noreply.github.com>
2017-12-12 10:13:44 +0900

    追加
    Unity Projectを追加

---
+++ Assets/Scripts/RemoteRotate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoteRotate : MonoBehaviour {
+    [SerializeField]
+    private int mRotation;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftControl))
+        {
+            Debug.Log("OK");
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Rotate(mRotation, 0, 0);
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Rotate(0, -mRotation, 0);
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Rotate(0, mRotation, 0);
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Rotate(-mRotation, 0, 0);
+            }
+        }
+	}
+}
